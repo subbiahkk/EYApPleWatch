9096bd3e2c37da9af50b03a80bc082a53ad82d43
subbiahkk <eydev@Thaos-MacBook-Pro.local>
2015-06-11 16:01:14 -0400

    include engagement cell

---
+++ EYAppleWatchPOC.IPhone/Views/EngagementCell.cs
@@ -0,0 +1,24 @@
+﻿
+using System;
+
+using Foundation;
+using UIKit;
+
+namespace EYAppleWatchPOC.IPhone
+{
+	public partial class EngagementCell : UITableViewCell
+	{
+		public static readonly UINib Nib = UINib.FromName ("EngagementCell", NSBundle.MainBundle);
+		public static readonly NSString Key = new NSString ("EngagementCell");
+
+		public EngagementCell (IntPtr handle) : base (handle)
+		{
+		}
+
+		public static EngagementCell Create ()
+		{
+			return (EngagementCell)Nib.Instantiate (null, null) [0];
+		}
+	}
+}
+
